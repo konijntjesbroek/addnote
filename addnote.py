8fe083e9c789a2e09e748ffbcddfbda18920a6a6
Arlo Gittings <>
2021-04-25 01:51:46 +0000

    timestamp, addnote formatting

--- addnote.py
+++ addnote.py
@@ -3,11 +3,13 @@ context = input('context: ')
 note = ''
 current_line = ''
 
-print('Enter note, use a . on a line by itself to end.\n')
+print('Enter note, use a . on a line by itself to end.')
 
 while(current_line != '.'):
     current_line = input('\t').strip()
     if (current_line != '.'):
         note += current_line + '\n' 
 
-print('Context: ' + context + '\nNote:\n\n' +note)
+current_time = time.strftime("%H%M", time.localtime())
+
+print('note added: ' + current_time + '\ncontext: ' + context + '\ndetails: \n' + note)
