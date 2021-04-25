4c8c35ccc90a3459f84596328aa3706fdab71b63
Arlo Gittings <>
2021-04-25 02:21:18 +0000

    validate configs

--- addnote.py
+++ addnote.py
@@ -1,15 +1,28 @@
 import time
-context = input('context: ')
-note = ''
-current_line = ''
+from os import path
 
-print('Enter note, use a . on a line by itself to end.')
+def main():
+    CFG_FILE = "notes.cfg"
+    CFG_PATH = ".cfg"
 
-while(current_line != '.'):
-    current_line = input('\t').strip()
-    if (current_line != '.'):
-        note += current_line + '\n' 
+    context = input('context: ')
+    note = ''
+    current_line = ''
 
-current_time = time.strftime("%H%M", time.localtime())
+    print('Enter note, use a . on a line by itself to end.')
 
-print('note added: ' + current_time + '\ncontext: ' + context + '\ndetails: \n' + note)
+    while(current_line != '.'):
+        current_line = input('\t').strip()
+        if (current_line != '.'):
+            note += current_line + '\n' 
+
+    current_time = time.strftime("%H%M", time.localtime())
+
+    print('note added: ' + current_time + 
+          '\ncontext: ' + context + 
+          '\ndetails: \n' + note
+    )
+def get_config(cfg_file)
+
+if __name__ == '__main__':
+    main()
